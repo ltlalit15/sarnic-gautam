b178f2a1d3cc0f13b9b81b702f2a1c6af3e962dc
Saif-Siddiqui16 <Saif-Siddiqui16@users.noreply.github.com>
2025-12-24 18:10:41 -0800

    created timelogs all employees

--- Config/dbConnect.js
+++ Config/dbConnect.js
@@ -36,8 +36,8 @@ export const pool = mysql.createPool({
 //   host: "localhost",            // ✅ Match CLI host
 //   port: 3306,                                // ✅ Match CLI port
 //   user: "root",                               // ✅ Match CLI user
-//   password: "Gopal_1820", // ✅ Match CLI password
-//   database: "sarnicdb",                        // ✅ Match CLI DB name
+//   password: "", // ✅ Match CLI password
+//   database: "sarnic_new",                        // ✅ Match CLI DB name
 //   waitForConnections: true,
 //   connectionLimit: 10,
 //   queueLimit: 0,
