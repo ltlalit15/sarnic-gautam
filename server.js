d8665d8948b81c35acb993fa91830a1d7a643c81
GautamBairagi <gauatambairagi22@gmail.com>
2025-12-26 17:06:54 +0530

    Mr Robot

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(
   cors({
     origin: [
       process.env.FRONTEND_URL, 
-      "http://localhost:5173",
+   
       "https://sanric-project.netlify.app",
     ].filter(Boolean) ,
     credentials: true,
